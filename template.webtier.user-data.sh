aafd742a22e42b5b02e2e2fc9b7bf532aa0ab4ae
Kevin Pedersen <byte_count@hotmail.com>
2015-08-17 23:15:09 -0400

    added sed replacement of centos user in sudoers with IAM user

--- template.webtier.user-data.sh
+++ template.webtier.user-data.sh
@@ -48,7 +48,7 @@ LHOST=`curl http://169.254.169.254/latest/meta-data/local-hostname/`
 IPADDR=`curl http://169.254.169.254/latest/meta-data/local-ipv4/`
 echo $IPADDR $LHOST $SHOST $HOSTNAME.local $HOSTNAME.private >> /etc/hosts
 
-# configure iam user/key
+# configure iam user/key and sudoers
 useradd kpedersen -d /home/kpedersen -s /bin/bash
 usermod -G wheel,adm kpedersen
 mkdir -p /home/kpedersen/.ssh
@@ -56,6 +56,7 @@ curl -s "http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key/" -o 
 chmod 700 /home/kpedersen/.ssh
 chmod 600 /home/kpedersen/.ssh/authorized_keys
 chown -R kpedersen:kpedersen /home/kpedersen/.ssh
+sed -i s/centos/kpedersen/g /etc/sudoers.d/*
 
 # clean up bash history
 cat /dev/null > /root/.bash_history
