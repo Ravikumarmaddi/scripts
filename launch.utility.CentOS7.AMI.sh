8a4ca13b83acba88453ade709b7444ec51084bab
Kevin Pedersen <byte_count@hotmail.com>
2015-08-23 19:24:04 -0400

    fixed spacing in PLATFORM variable

--- launch.utility.CentOS7.AMI.sh
+++ launch.utility.CentOS7.AMI.sh
@@ -31,7 +31,7 @@ sed -i s/UtilityInstance/${USERHOST}/g $USERDATA
 # specify machine details
 IMAGE="ami-c7d092f7"
 SCRIPT="file://$USERDATA"
-PLATFORM="CentOS 7"
+PLATFORM="CentOS7"
 TIER="Utility"
 TYPE="t2.micro"
 GROUP="UtilityTierNetworkAccess"
