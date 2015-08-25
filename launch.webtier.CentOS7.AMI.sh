f8c658c067313380abf667953c5c0d936eb2762b
Kevin Pedersen <byte_count@hotmail.com>
2015-08-24 21:01:58 -0400

    minor updates

--- launch.webtier.CentOS7.AMI.sh
+++ launch.webtier.CentOS7.AMI.sh
@@ -26,7 +26,7 @@ fi
 # create template
 USERDATA=`mktemp`
 cp ~/scripts/template.webtier.user-data.sh $USERDATA
-sed -i s/WebInstance/${USERHOST}/g $USERDATA
+sed -i s/WebInstance/${USERHOST}/ $USERDATA
 
 # specify machine details
 IMAGE="ami-c7d092f7"
