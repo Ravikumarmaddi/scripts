7e4f4a7fd3ff14e6f58a7a0321c1649f1847636f
Kevin Pedersen <byte_count@hotmail.com>
2015-08-24 22:00:15 -0400

    created command strings for testing and removed monitoring section

--- launch.datatier.user-data.sh
+++ launch.datatier.user-data.sh
@@ -37,7 +37,7 @@ PLATFORM="MySQL"
 USER=`echo $USERDB | awk '{print tolower($0)}'`
 PASSWD=`echo $USERDB | awk '{print tolower($0)}'`
 TIER="Database"
-HOSTGROUP="DatabaseTierNetworkAccess"
+GROUP="DatabaseTierNetworkAccess"
 VPCGROUPS='"sg-4dc26f29" "sg-2056fd44"'
 REGION="us-west-2"
 RETPERIOD=0
@@ -55,7 +55,7 @@ fi
 ///need to finish this
 
 # construct parameters
-EXEC="--image-id $IMAGE --key-name $KEY --user-data $SCRIPT --instance-type $TYPE --security-groups $GROUP --iam-instance-profile Name=$ROLE --region $REGION"
+EXEC="--db-name $DBNAME --db-instance-identifier $DBIID --allocated-storage $DBSIZE --db-instance-class $DBTYPE --engine $PLATFORM --master-username $USER --master-user-password $PASSWD --db-security-groups $GROUP --vpc-security-group-ids $VPCGROUPS --backup-retention-period $RETPERIOD --no-publicly-accessible"
 
 # execute launch command with parameters
 OUT=`mktemp`
@@ -63,58 +63,14 @@ echo $COMMAND $EXEC
 $COMMAND $EXEC | tee $OUT
 
 # grab the instance id
-IID=`cat $OUT | grep InstanceId | awk '{print $2}' | sed s/\"//g | sed s/,//g`
+#IID=`cat $OUT | grep InstanceId | awk '{print $2}' | sed s/\"//g | sed s/,//g`
 
 # tag the instance
-echo "Tagging instance..."
-aws ec2 create-tags --resources $IID --tags Key=Name,Value=$USERHOST Key=Platform,Value=$PLATFORM Key=Tier,Value=$TIER
+#echo "Tagging instance..."
+#aws ec2 create-tags --resources $IID --tags Key=Name,Value=$USERDB Key=Platform,Value=$PLATFORM Key=Tier,Value=$TIER
 
 # monitor the instance
-echo -n "Monitoring instance... "
-echo -n "HighCPU... "
-aws cloudwatch put-metric-alarm \
---alarm-name $USERHOST.HighCPU \
---metric-name CPUUtilization \
---namespace AWS/EC2 \
---statistic Average \
---period 300 \
---threshold 80 \
---comparison-operator GreaterThanThreshold \
---dimensions Name=InstanceID,Value=$IID \
---evaluation-periods 1 \
---unit Percent \
---alarm-actions arn:aws:sns:us-west-2:035296091979:administrator
-
-echo -n "HighOutboundTraffic... "
-aws cloudwatch put-metric-alarm \
---alarm-name $USERHOST.HighOutboundTraffic \
---metric-name NetworkOut \
---namespace AWS/EC2 \
---statistic Average \
---period 300 \
---threshold 50000000 \
---comparison-operator GreaterThanThreshold \
---dimensions Name=InstanceID,Value=$IID \
---evaluation-periods 1 \
---unit Bytes \
---alarm-actions arn:aws:sns:us-west-2:035296091979:administrator
-
-echo -n "StatusCheckFailed... "
-aws cloudwatch put-metric-alarm \
---alarm-name $USERHOST.StatusCheckFailed \
---metric-name StatusCheckFailed \
---namespace AWS/EC2 \
---statistic Maximum \
---period 300 \
---threshold 1 \
---comparison-operator GreaterThanOrEqualToThreshold \
---dimensions Name=InstanceID,Value=$IID \
---evaluation-periods 1 \
---unit Count \
---alarm-actions arn:aws:sns:us-west-2:035296091979:administrator
-
-echo
 
 # clean up temp files
-rm -f $OUT $USERDATA
+rm -f $OUT 
 
